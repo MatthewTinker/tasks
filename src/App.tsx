0286b9f08325ae0691375125fef661f539bbd1d8
MatthewTinker <matthew.tinkham@maine.edu>
2025-02-13 11:49:42 -0500

    Added name to App

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): React.JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UM COS420 with React Hooks and TypeScript
+                UM COS420 WOWIE MATTHEW TINKHAM with React Hooks and TypeScript
             </header>
             <hr></hr>
             <Counter></Counter>
