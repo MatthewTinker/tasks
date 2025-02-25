21b40859dfcd5b481755b0681df19dcb61044ef2
MatthewTinker <matthew.tinkham@maine.edu>
2025-02-25 14:26:35 -0500

    stringsToIntegers

--- src/arrays.ts
+++ src/arrays.ts
@@ -25,7 +25,10 @@ export function tripleNumbers(numbers: number[]): number[] {
  * the number cannot be parsed as an integer, convert it to 0 instead.
  */
 export function stringsToIntegers(numbers: string[]): number[] {
-    return [];
+    return numbers.map(string => {
+        let num: number = parseInt(string);
+        return isNaN(num) ? 0: num;
+    });
 }
 
 /**
